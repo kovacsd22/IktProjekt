bc58e0ad39f98648792df09a32c1737f2d869835
kovacsd22 <112945179+kovacsd22@users.noreply.github.com>
2023-10-03 07:33:17 +0200

    database

---
+++ 127_0_0_1(1).sql
@@ -0,0 +1,77 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2023. Okt 03. 07:32
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Adatbázis: `webshop`
+--
+DROP DATABASE IF EXISTS `webshop`;
+CREATE DATABASE IF NOT EXISTS `webshop` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_hungarian_ci;
+USE `webshop`;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `felhasznalok`
+--
+
+CREATE TABLE `felhasznalok` (
+  `id` int(11) NOT NULL,
+  `LoginNev` varchar(16) NOT NULL,
+  `HASH` varchar(64) NOT NULL,
+  `SALT` varchar(64) NOT NULL,
+  `Nev` varchar(64) NOT NULL,
+  `Jog` int(1) NOT NULL,
+  `Aktiv` tinyint(1) NOT NULL,
+  `Email` varchar(64) NOT NULL,
+  `ProfilKep` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `felhasznalok`
+--
+
+INSERT INTO `felhasznalok` (`id`, `LoginNev`, `HASH`, `SALT`, `Nev`, `Jog`, `Aktiv`, `Email`, `ProfilKep`) VALUES
+(1, 'Yrim', 'kecske', 'ekcsek', 'Dani', 1, 0, 'kecske@kecske.com', 'jpg.jpg'),
+(4, 'cigan', 's', 's', 'Olá', 3, 1, 'olahtarsa@kft.com', 'olahtarsa.jpg');
+
+--
+-- Indexek a kiírt táblákhoz
+--
+
+--
+-- A tábla indexei `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `LoginNev` (`LoginNev`);
+
+--
+-- A kiírt táblák AUTO_INCREMENT értéke
+--
+
+--
+-- AUTO_INCREMENT a táblához `felhasznalok`
+--
+ALTER TABLE `felhasznalok`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
